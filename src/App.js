fba2b318d8f2cb74cb564ae61188e4201f6f13e1
Sandy <peacesandy04@gmail.com>
2023-04-06 14:49:02 -0700

    Adde react router Dom

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Link from './Link'
 import Logo from './Logo'
 import Header from './Header'
 import Footer from './Footer'
+import Navigation from './Navigation'
 
 
 function App() {
   return (
     <div className="App">
+      <Navigation/>
       <Header/>
       <Logo/>
       <Button/>
