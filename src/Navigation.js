71ba02fb4668f8f4fe1fd3f31038b6babd067526
Sandy <peacesandy04@gmail.com>
2023-04-06 15:03:37 -0700

    Added Nav Link

---
+++ src/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { NavLink} from 'react-router-dom'
+
+
+function Navigation(){
+   <nav>
+    <ul>
+    <li><NavLink to='/'>Gmail</NavLink></li>
+    <li><NavLink to='/'>Images</NavLink></li>
+    </ul>
+   </nav>
+}
+
+export default Navigation
