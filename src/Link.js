be1f0822a634f714a40864f8ade1a2db40a9e219
Sandy <peacesandy04@gmail.com>
2023-04-07 05:27:05 -0700

    styled link

--- src/Link.js
+++ src/Link.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import './styles/Link.css'
 
 function Link() {
     return(
-        <div> 
+        <div className='link'> 
          <p>Goggle offered in <a href='#'>Hausa Igbo Ede Yoruba Nigerian Pidgin</a></p>
         </div>
     )
