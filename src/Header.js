5b0dadd9cd227355e86c9100c974e0c427f4feeb
Sandy <peacesandy04@gmail.com>
2023-04-07 06:01:03 -0700

    rounding up

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ import profilePhoto from './google image.jpg'
 
 function Header() {
     return(
-        <div> 
+        <div className='header'> 
             <p>Gmail</p>
             <p>Images</p>
             <img src={profilePhoto} className='profile-photo' alt='Profile Photo'/>
